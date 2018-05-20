fc148530b330319deb0e42e07dcabe28546b1daa
Mike A <addonisio.m@gmail.com>
2018-05-20 17:38:05 -0600

    binary cross

--- cnn_model.py
+++ cnn_model.py
@@ -1,7 +1,7 @@
 from keras.models import Model
 from keras import layers
 from keras.layers import Activation, Dense, Dropout, Flatten, Input, merge, \
-                         Conv2D, Concatenate
+                         Conv2D, Concatenate, LSTM, Conv1D, Reshape, Permute
 from keras.callbacks import ModelCheckpoint
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 import numpy as np
@@ -12,6 +12,7 @@ import exploratory as tsa
 import keras
 import pdb
 from skimage.transform import resize
+import tensorflow as tf
 
 import cv2
 
@@ -79,24 +80,24 @@ def AlexNet(weights_path=None):
 
     return model
 
-def generator2(subjects, label_path, batch_size):
+def generator3channel(subjects, label_path, batch_size):
     # intialize tracking and saving items
+    files = [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f))]
     threat_zone_examples = []
     labels = get_subject_labels(label_path)
     print_shape = True
-    subjects = [subject.split('.')[0] for subject in subjects]
     while True:
-        for i in range(0, len(subjects), batch_size):
+        for i in range(0, len(files), batch_size):
             y_batch = []
             features = {}
             for j in range(0, 16):
                 features[str(j)] = []
-            for subject in subjects[i:i+batch_size]:
-                images = tsa.read_data('/Users/michaeladdonisio/Documents/Galvanize/case-studies/tsa_screen/data/train' + '/' + subject + '.aps')
+            for file in files[i:i+batch_size]:
+                images = tsa.read_data(os.path.join(src, file))
                 # transpose so that the slice is the first dimension shape(16, 620, 512)
                 images = images.transpose()
                 for j in range(0, 16):
-                    image = resize(images[j], (82, 64))
+                    image = resize(images[j], input_size)
                     image = image[:,:,np.newaxis]
                     """if print_shape:
                         print ("Shape of re-transposed image:")
@@ -109,7 +110,7 @@ def generator2(subjects, label_path, batch_size):
 
                 # get label
                 y = np.zeros((17))
-                threat_list = labels.loc[labels['Subject'] == subject.split(".")[0]]
+                threat_list = labels.loc[labels['Subject'] == file.split(".")[0]]
                 threat_iter = threat_list.iterrows()
                 while True:
                     threat = next(threat_iter, None)
@@ -188,130 +189,6 @@ def generator(src, label_path, input_size=(155,128), batch_size=1):
 
             yield features, np.array(y_batch)
 
-def plot_loss_acc():
-    # plot the training loss and accuracy
-    plt.style.use("ggplot")
-    plt.figure()
-    N = EPOCHS
-    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-    plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-    plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
-    plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
-    plt.title("Training Loss and Accuracy")
-    plt.xlabel("Epoch #")
-    plt.ylabel("Loss/Accuracy")
-    plt.legend(loc="upper left")
-    plt.savefig(args["plot"])
-
-def plot_loss_acc2(hist):
-    acc = hist.history['acc']
-    val_acc = hist.history['val_acc']
-    loss = hist.history['loss']
-    val_loss = hist.history['val_loss']
-
-    epochs = range(len(acc))
-
-    plt.plot(epochs, acc, 'bo', label='Training acc')
-    plt.plot(epochs, val_acc, 'b', label='Validation acc')
-    plt.title('Training and validation accuracy')
-    plt.legend()
-
-    plt.figure()
-
-    plt.plot(epochs, loss, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.legend()
-
-    plt.show()
-
-def train_binary_net():
-
-    batch_size = 1
-
-    get_train_test_file_list()
-    test_subjects =TEST_SUBJECT_LIST# get_relevant_subjects(TEST_SUBJECT_LIST, THREAT_ZONE)
-    train_subjects =TRAIN_SUBJECT_LIST# get_relevant_subjects(TRAIN_SUBJECT_LIST, THREAT_ZONE)
-
-    test_gen = generator(test_subjects, batch_size)
-    train_gen = generator(train_subjects, batch_size)
-
-    #test_gen = generator(TEST_SUBJECT_LIST, batch_size)
-    #train_gen = generator(TRAIN_SUBJECT_LIST, batch_size)
-    print("train_gen info:")
-    print(np.array(next(train_gen)[1]).shape)
-    print(next(train_gen)[1])
-    print ("data lengths:")
-    print (len(train_subjects))
-    print (len(test_subjects))
-
-
-    train_steps = np.ceil(float(len(train_subjects)) / float(batch_size))
-    test_steps = np.ceil(float(len(test_subjects)) / float(batch_size))
-
-    LR = 0.01
-
-    model = VGG_16()#MVCNN()
-    #model.load_weights(MVCNN_PATH)
-
-    print ("LR is " + str(LR))
-    model.compile(loss=keras.losses.binary_crossentropy,
-            optimizer= keras.optimizers.AdamAccum(lr=LR, accumulator=32.0), metrics=['acc'])
-    mvcnn_checkpoint = ModelCheckpoint(MVCNN_PATH, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    model.fit_generator(generator=train_gen, validation_data=test_gen, steps_per_epoch = train_steps, validation_steps = test_steps,
-        epochs = 1000, verbose=2, callbacks=[mvcnn_checkpoint])
-
-    """for i in range(0, 15):
-
-        im, label = next(train_gen)
-
-        print("imshape, label:")
-        print (im.shape)
-        print (label)
-
-    print ("lengths:")
-    print(len(test_subjects))
-    print(len(train_subjects))
-    # get train and test batches
-    get_train_test_file_list()
-    features, labels = get_dataset(TRAIN_SET_FILE_LIST, PREPROCESSED_DATA_FOLDER)
-    val_features, val_labels = get_dataset(TEST_SET_FILE_LIST, PREPROCESSED_DATA_FOLDER)
-    features = features.reshape(-1, IMAGE_DIM, IMAGE_DIM, 3)
-    labels = labels.reshape(-1, 2)
-    val_features = val_features.reshape(-1, IMAGE_DIM, IMAGE_DIM, 3)
-    val_labels = val_labels.reshape(-1, 2)
-    print (features[0].shape)
-    # instantiate model
-    #model = alexnet(IMAGE_DIM, IMAGE_DIM, LEARNING_RATE)
-    log = model.fit(x=features, y=labels, batch_size=1, epochs=10000, verbose=2, validation_data=(val_features, val_labels))
-    """
-    return
-
-def precision(y_true, y_pred):
-    """Precision metric.
-
-    Only computes a batch-wise average of precision.
-
-    Computes the precision, a metric for multi-label classification of
-    how many selected items are relevant.
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
-
-def recall(y_true, y_pred):
-    """Recall metric.
-
-    Only computes a batch-wise average of recall.
-
-    Computes the recall, a metric for multi-label classification of
-    how many relevant items are selected.
-    """
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
 
 def MVCNN_small(input_size=(155,128),weights_path=None):
 
@@ -359,7 +236,7 @@ def MVCNN_small(input_size=(155,128),weights_path=None):
         view_pool.append(new_model)
         """
     vp = Concatenate(axis=0)(view_pool) #tf.concat([vp, v], 0)
-    model = Dense(32, activation='relu', kernel_initializer='glorot_normal')(vp)
+    model = Dense(34, activation='relu', kernel_initializer='glorot_normal')(vp)
     #model = Dropout(0.2)(model)
     #model = Dropout(0.2)(model)
     #model = Flatten()(model)
@@ -371,7 +248,7 @@ def MVCNN_small(input_size=(155,128),weights_path=None):
 
     full_model = Model(inputs=inputs, outputs=model)
 
-    full_model.compile(loss=keras.losses.categorical_crossentropy,
+    full_model.compile(loss=keras.losses.binary_crossentropy,
             optimizer= keras.optimizers.Adam(lr=0.01), metrics=['accuracy'])
 
     full_model.summary()
@@ -449,6 +326,44 @@ def MVCNN(input_size=(155,128), weights_path=None):
 
     return full_model
 
+def MVCNN_lstm(input_size=(155,128), weights_path=None):
+        inputs = []
+        view_pool = []
+
+        for i in range(0, 16):
+            new_input = Input(input_size + (1,), name=str(i)) #make new input
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(new_input)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = MaxPooling2D((2,2), strides=(2,2))(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = MaxPooling2D((2,2), strides=(2,2))(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = MaxPooling2D((2,2), strides=(2,2))(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = Conv2D(32, (3, 3), activation='relu', kernel_initializer='glorot_normal')(x)
+            x = MaxPooling2D((2,2), strides=(2,2))(x)
+            x = Flatten()(x)
+            inputs.append(new_input)
+            view_pool.append(x)
+        vp = Concatenate(axis=1)(view_pool) #tf.concat([vp, v], 0)
+        pdb.set_trace()
+        vp = Reshape((-1,-1,-1))*vp
+        model = LSTM(34)(vp)
+        model = Dense(17, activation='sigmoid', kernel_initializer='glorot_normal')(model)
+
+        full_model = Model(inputs=inputs, outputs=model)
+
+        full_model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer= keras.optimizers.Adam(lr=0.01), metrics=['accuracy'])
+
+        full_model.summary()
+
+        return full_model
+
+
 def test_generator(test_path):
     # intialize tracking and saving items
     files = [f for f in os.listdir(test_path) if os.path.isfile(os.path.join(test_path, f))]
@@ -488,15 +403,16 @@ def model_eval(model, train_path, val_path, label_path, input_size, batch_size, 
     val_gen = generator(val_path, label_path, input_size, batch_size)
     val_steps = np.ceil(float(len(val_files)) / float(batch_size))
     checkpointer = ModelCheckpoint(filepath='/tmp/weights.hdf5', verbose=1, save_best_only=True)
-    history = model.fit_generator(generator=train_gen, validation_data=val_gen, steps_per_epoch = train_steps, validation_steps = val_steps,epochs = epochs, verbose=2, callbacks=[checkpointer])
+    history = model.fit_generator(generator=train_gen, validation_data=val_gen, steps_per_epoch = train_steps, validation_steps = val_steps,epochs = epochs, verbose=1, callbacks=[checkpointer])
     if not None:
         model.save('models/' + name + '.h5')
     return model, history
 
-
-
 def main():
-    input_size= (300,300)
+    # input_size= (620, 512)
+    # input_size= (310, 256)
+    input_size= (155, 128)
+    # input_size= (100,100)
     batch_size = 1
     epochs = 1
     model = MVCNN_small(input_size)
